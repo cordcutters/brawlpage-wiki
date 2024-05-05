feee76eba1340f944c115afb3211b26da3d63bb4
Jonas <140553581+jnsdotgg@users.noreply.github.com>
2024-05-05 23:31:33 +0200

    dsa

--- graveyard/main.js
+++ graveyard/main.js
@@ -4,12 +4,10 @@ const items = document.getElementById("items");
 
 list.forEach((item) => {
   const element = document.createElement("div");
-  element.className = "bg-white/80 backdrop-blur text-black py-6 px-10 gap-4";
-  element.style =
-    "clip-path: polygon(1rem 0%, 100% 0%, calc(100% - 1rem) 100%, 0% 100%);";
+  element.className = "bg-white/80 backdrop-blur text-black p-6 gap-4";
   element.innerHTML = `
     <div class="flex gap-4 mb-4">
-      <img src="${item.image}" class="size-14">
+      <img src="${item.image}" class="size-14 object-contain">
       <div>
         <h3 class="text-2xl">${item.name}</h3>
         <h4>${item.added} - ${item.removed}</h4>
