de060238a532103467667a1f541143c3caf14d84
Jonas <140553581+jnsdotgg@users.noreply.github.com>
2024-05-05 23:36:20 +0200

    ds

--- graveyard/list.js
+++ graveyard/list.js
@@ -3,7 +3,7 @@ export const list = [
     name: "Star Points",
     image: "./assets/images/star-point.webp",
     description:
-      "Star Points Star Points were a currency that could be earned by achieving certain ranks for any Brawler and at the end of every Trophy League season, every 5 ranks achieved (from rank 10) and Power League season or through certain Quests or Challenges. Star Points were used to buy exclusive skins. Unlike Gems, they could not be purchased with in-app purchases.",
+      "Star Points were a currency you would get at the end of Trophy League, used to buy cosmetics ",
     added: "26 Jun 2019",
     removed: "25 Apr 2023",
   },
@@ -12,7 +12,7 @@ export const list = [
     image:
       "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/icon_chroma_credits.png",
     description:
-      "Chroma Credits were an in-game currency similar to Credits. Unlike Credits, Chroma Credits could not be obtained from Starr Drops. Chroma Credits could be used to unlock Chromatic Brawlers of a player's choosing. When you got every Brawler, Chroma Credits would be used for achieving Fame if a player unlocked all Chromatic Brawlers. A maximum of 5000 Chroma Credits could be held.",
+      "Chroma Credits were a currency used to unlock Chromatic Brawlers",
     added: "12 Dec 2022",
     removed: "12 Dec 2023",
   },
@@ -21,7 +21,7 @@ export const list = [
     image:
       "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/starpower_base02.png",
     description:
-      "Pat on the Back was Gene's Star Power that healed his teammates when using his super on them.",
+      "Pat on the Back was Gene's Star Power that healed his teammates when using his super on them",
     added: "7 Aug 2019",
     removed: "29 Aug 2019",
   },
@@ -30,7 +30,7 @@ export const list = [
     image:
       "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/robot_icon.png",
     description:
-      "Siege in Brawl Stars was a gamemode mode where you collect Bolts to summon a Siege Robot and attack the enemy IKE while defending their own.",
+      "Siege was a gamemode mode where you collect Bolts to summon a Siege Robot and attack the enemy IKE while defending your own",
     added: "27 Feb 2019",
     removed: "31 Sep 2022",
   },
