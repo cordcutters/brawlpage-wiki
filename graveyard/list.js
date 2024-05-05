1401dd8163147a93d212458e2cb411d12e2083e5
Jonas <140553581+jnsdotgg@users.noreply.github.com>
2024-05-06 01:40:28 +0200

    up

--- graveyard/list.js
+++ graveyard/list.js
@@ -1,13 +1,5 @@
 export const list = [
   {
-    name: "Star Points",
-    image: "./assets/images/star-point.webp",
-    description:
-      "Star Points were a currency you would get at the end of Trophy League, used to buy cosmetics ",
-    added: "26 Jun 2019",
-    removed: "25 Apr 2023",
-  },
-  {
     name: "Chroma Credits",
     image:
       "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/icon_chroma_credits.png",
@@ -17,13 +9,38 @@ export const list = [
     removed: "12 Dec 2023",
   },
   {
-    name: "Pat on the Back",
+    name: "Pin Packs",
     image:
-      "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/starpower_base02.png",
+      "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/pin_pack_icon.png",
     description:
-      "Pat on the Back was Gene's Star Power that healed his teammates when using his super on them",
-    added: "7 Aug 2019",
-    removed: "29 Aug 2019",
+      "You could buy Pin Packs for 49 gems from the shop, they would contain 2 random pins",
+    added: "13 May 2020",
+    removed: "12 Jun 2023",
+  },
+  {
+    name: "Star Points",
+    image: "./assets/images/star-point.webp",
+    description:
+      "Star Points were a currency you would get at the end of Trophy League, used to buy cosmetics ",
+    added: "26 Jun 2019",
+    removed: "25 Apr 2023",
+  },
+  {
+    name: "Experience",
+    image: "./assets/images/experience.webp",
+    description:
+      "You would get experience after each battle. Unlike Trophies, Experience could not be lost. reaches. When a player leveled up, they received 20 Tokens as a reward. ",
+    added: "14 Jun 2017",
+    removed: "28 Feb 2023",
+  },
+  {
+    name: "Boxes",
+    image:
+      "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/darryl_megabox_pin.png",
+    description:
+      "Brawl Boxes were the main way of progression, you could get Power Points, coins, and even gems at some point from these",
+    added: "14 Jun 2017",
+    removed: "12 Dec 2022",
   },
   {
     name: "Siege",
@@ -35,12 +52,55 @@ export const list = [
     removed: "31 Sep 2022",
   },
   {
-    name: "Boxes",
+    name: "Surge's old Power Surge",
     image:
-      "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/darryl_megabox_pin.png",
+      "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/8bit_gadget_01.png",
     description:
-      "Brawl Boxes were the main way of progression, you could get Power Points, coins, and even gems at some point from these",
-    added: "14 Jun 2017",
-    removed: "10 Dec 2022",
+      "Surge's old Power Surge gadget would make him teleport 3 tiles ahead",
+    added: "6 Jul 2020",
+    removed: "27 Apr 2022",
+  },
+  {
+    name: "Lone Star",
+    image:
+      "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/lone_star_icon.png",
+    description: "",
+    added: "18 Sep 2019",
+    removed: "23 May 2021",
+  },
+  {
+    name: "8-Bit's Extra Life",
+    image:
+      "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/8bit_gg_pin.png",
+    description:
+      "8-Bit used to have a star power that would make him respawn when dying at the same location, even in Showdown",
+    added: "29 Aug 2019",
+    removed: "10 Sep 2020",
+  },
+  {
+    name: "Tickets",
+    image: "./assets/images/ticket.webp",
+    description:
+      "Tickets were used to play special weekend events. The more tickets you would spend the more rewards you would get ",
+    added: "7 Dec 2017",
+    removed: "13 May 2020",
+  },
+  {
+    name: "Gene's Pat on the Back",
+    image:
+      "https://laptopcat.github.io/bs-fankit-rehosted/game-assets/gene_starpower_02.png",
+    description:
+      "Pat on the Back was one of Gene's Star Powers that healed his teammates when using his super on them",
+    added: "7 Aug 2019",
+    removed: "29 Aug 2019",
+  },
+  {
+    name: "Life Leech",
+    image: "./assets/images/life-leech.png",
+  },
+  {
+    name: "Pins, badges, medals upgrading system",
+    description:
+      "You used to upgrade your brawlers using pins, badges and medals that you would get from boxes",
   },
 ];
--- graveyard/main.js
+++ graveyard/main.js
@@ -1,19 +1,27 @@
 import { list } from "./list.js";
 
-const items = document.getElementById("items");
-
 list.forEach((item) => {
   const element = document.createElement("div");
-  element.className = "bg-white/80 backdrop-blur text-black p-6 gap-4";
+  element.className = "bg-white/80 rounded-xl text-black p-6 gap-4";
   element.innerHTML = `
     <div class="flex gap-4 mb-4">
-      <img src="${item.image}" class="size-14 object-contain">
+    ${
+      item.image
+        ? `<img src="${item.image}" class="size-14 object-contain">`
+        : ""
+    }
       <div>
         <h3 class="text-2xl">${item.name}</h3>
-        <h4>${item.added} - ${item.removed}</h4>
+        ${
+          item.added && item.removed
+            ? `<h4>${item.added} - ${item.removed}</h4>`
+            : ""
+        }
       </div>
     </div>
-    <p class="font-sans col-span-2">${item.description}</p>
+    <p class="font-sans col-span-2">
+    ${item.description ? item.description : "No description"}
+    </p>
   `;
   items.append(element);
   console.log(item.name);
